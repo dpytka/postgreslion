983ba2f11538070e1ee574bac48e224470042cac
dawid <dpytka@gmail.com>
2013-03-20 19:06:09 +0100

    angularjs

--- static/js/databases.js
+++ static/js/databases.js
@@ -1,18 +1,15 @@
 function DatabasesCtrl($scope) {
 
-    $scope.socket = io.connect();
-    $scope.socket.on('database_created', function (data) {
-        $scope.dbs.push({url: data.url});
-        $scope.update();
+    var socket = io.connect();
+    socket.on('database_created', function (data) {
+        $scope.$apply(function(){
+            $scope.dbs.push({url: data.url});
+        });
     });
-
-    $scope.update = function() {
-
-    };
 
     $scope.dbs = [];
 
     $scope.addDb = function () {
-        $scope.socket.emit('create_db', 'Message Sent on ' + new Date());
+        socket.emit('create_db', 'Message Sent on ' + new Date());
     };
 }
